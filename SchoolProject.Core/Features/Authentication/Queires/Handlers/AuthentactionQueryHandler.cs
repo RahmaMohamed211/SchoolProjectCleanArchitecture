ec8b35ce2a01a9d02c13aac50b9c6e7a96a01e65
Rahma Mohamed <116441676+RahmaMohamed211@users.noreply.github.com>
2025-01-18 00:49:35 +0200

    Authentication (#6)
    * Added LoginEndPoint
    
    * Added RefershToken
    
    * Added RefreshToken EndPoint
    
    ---------
    
    Co-authored-by: rahma.sw2021 <rahma.sw2021@fci.helwan.edu.eg>

---
+++ SchoolProject.Core/Features/Authentication/Queires/Handlers/AuthentactionQueryHandler.cs
@@ -0,0 +1,48 @@
+﻿using MediatR;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.Extensions.Localization;
+using SchoolProject.Core.Bases;
+using SchoolProject.Core.Features.Authentication.Command.Models;
+using SchoolProject.Core.Features.Authentication.Queires.Models;
+using SchoolProject.Core.Resources;
+using SchoolProject.Data.Entities.Identity;
+using SchoolProject.Data.Helpers;
+using SchoolProject.Service.Abstracts;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SchoolProject.Core.Features.Authentication.Queires.Handlers
+{
+    public class AuthentactionQueryHandler : ResponseHandler,
+        IRequestHandler<AuthorizeUserQuery, Response<string>>
+    {
+        #region fields
+        private readonly IStringLocalizer<SharedResources> _stringLocalizer;
+        private readonly IAuthenicationService _authenicationService;
+
+        #endregion
+        #region ctor
+        public AuthentactionQueryHandler(IAuthenicationService authenicationService, IStringLocalizer<SharedResources> stringLocalizer) : base(stringLocalizer)
+        {
+            _authenicationService = authenicationService;
+            
+            _stringLocalizer = stringLocalizer;
+        }
+        #endregion
+        #region functions
+        public async Task<Response<string>> Handle(AuthorizeUserQuery request, CancellationToken cancellationToken)
+        {
+            var result = await _authenicationService.ValidateToken(request.AccessToken);
+            if (result == "NotExpired") 
+                return Success(result);
+            return BadRequest<string>("Expired");
+        }
+        #endregion
+
+    
+
+    }
+}
--- SchoolProject.Service/Implementations/AuthenicationService.cs
+++ SchoolProject.Service/Implementations/AuthenicationService.cs
@@ -14,43 +14,44 @@ using System.Collections.Concurrent;
 using static SchoolProject.Data.Helpers.JwtAuthResult;
 using System.Security.Cryptography;
 using SchoolProject.infrastructure.Abstract;
+using Azure.Core;
+using System.Diagnostics.CodeAnalysis;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.AspNetCore.Identity;
 
 namespace SchoolProject.Service.Implementations
 {
     public class AuthenicationService : IAuthenicationService
     {
 
+
         #region fields
-       
+        private readonly UserManager<User> _userManager;
         private readonly JwtSettings _jwtSettings;
         private readonly IRefershTokenRepository _refershTokenRepository;
-        private readonly ConcurrentDictionary<string, RefreshToken> _userRefreshToken;
+        
         #endregion
         #region ctor
-        public AuthenicationService(JwtSettings jwtSettings,IRefershTokenRepository refershTokenRepository)
+        public AuthenicationService(UserManager<User> userManager,JwtSettings jwtSettings,IRefershTokenRepository refershTokenRepository)
         {
-            
+            _userManager = userManager;
             _jwtSettings = jwtSettings;
             _refershTokenRepository = refershTokenRepository;
-            _userRefreshToken = new ConcurrentDictionary<string, RefreshToken>();
+           
         }
 
         #endregion
         #region function
         public async Task< JwtAuthResult> GetJWTToken(User user)
         {
-            var claims= GetClaims(user);
-            var JwtToken = new JwtSecurityToken(
-                _jwtSettings.Issuser,_jwtSettings.Audience,
-                claims, expires: DateTime.UtcNow.AddDays(_jwtSettings.AccessTokenExpireDate),
-                signingCredentials: new SigningCredentials(new SymmetricSecurityKey(Encoding.ASCII.GetBytes(_jwtSettings.Secret)), SecurityAlgorithms.HmacSha256Signature));
-            var accessToken= new JwtSecurityTokenHandler().WriteToken(JwtToken);
-            var refershToken= GetRefreshToken(user.UserName);
+           
+          var(JwtToken, accessToken) =  GenerateJWTToken(user);
+             var refershToken = GetRefreshToken(user.UserName);
             var userRefershToken = new UserRefreshToken
             {
                 AddedTime = DateTime.Now,
                 ExpiryDate= DateTime.Now.AddDays(_jwtSettings.RefreshTokenExpireDate),
-                IsUsed= false,
+                IsUsed= true,
                 IsRevoked=false,
                 JWtId= JwtToken.Id,
                 RefreshToken=refershToken.TokenString,
@@ -76,7 +77,7 @@ namespace SchoolProject.Service.Implementations
                 TokenString = GenerateRefershToken()
 
             };
-            _userRefreshToken.AddOrUpdate(refreshToken.TokenString, refreshToken, (s, t) => refreshToken);
+           
             return refreshToken;
         }
         private string GenerateRefershToken()
@@ -93,8 +94,116 @@ namespace SchoolProject.Service.Implementations
                 new Claim(nameof(UserClaimModel.UserName),user.UserName),
                 new Claim(nameof(UserClaimModel.Email),user.Email),
                 new Claim(nameof(UserClaimModel.PhoneNumber),user.PhoneNumber),
+                new Claim(nameof(UserClaimModel.Id),user.Id.ToString()),
             };
             return claims;
+        }
+
+        private (JwtSecurityToken,string) GenerateJWTToken(User user)
+        {
+            var claims = GetClaims(user);
+            var JwtToken = new JwtSecurityToken(
+                _jwtSettings.Issuser, _jwtSettings.Audience,
+                claims, expires: DateTime.UtcNow.AddDays(_jwtSettings.AccessTokenExpireDate),
+                signingCredentials: new SigningCredentials(new SymmetricSecurityKey(Encoding.ASCII.GetBytes(_jwtSettings.Secret)), SecurityAlgorithms.HmacSha256Signature));
+            var accessToken = new JwtSecurityTokenHandler().WriteToken(JwtToken);
+            return (JwtToken, accessToken);
+        }
+
+        public async Task<JwtAuthResult> GetRefreshToken(string accessToken, string refreshToken)
+        {
+            //read token to get cliams
+            var token = ReadJWTToken(accessToken);
+            if (token == null ||!token.Header.Alg.Equals(SecurityAlgorithms.HmacSha256Signature))
+            {
+                throw new SecurityTokenException("Algrothim is wrong");
+            }
+            if (token.ValidTo > DateTime.UtcNow)
+            {
+                throw new SecurityTokenException("Token is not Expired");
+            }
+            //get user
+            var userId = 2;
+          //  var userId = token.Claims.FirstOrDefault(x => x.Type == nameof(UserClaimModel.Id)).Value;
+           var userRefreshToken= await _refershTokenRepository.GetTableNoTracking()
+                                             .FirstOrDefaultAsync(x=>x.Token==accessToken&&
+                                                                     x.RefreshToken==refreshToken
+                                                                     && x.UserId==4);
+            if (userRefreshToken == null) 
+            {
+                throw new SecurityTokenException("Refresh Token is Not found");
+            }
+            //validations token,refreshtoken
+            if (userRefreshToken.ExpiryDate < DateTime.UtcNow)
+            {
+                userRefreshToken.IsRevoked = true;
+                userRefreshToken.IsUsed = false; 
+                await _refershTokenRepository.UpdateAsync(userRefreshToken);
+                throw new SecurityTokenException(" Refresh Token is  Expired");
+            }
+            var user= await _userManager.FindByIdAsync("4");
+            if (user == null)
+            {
+                throw new SecurityTokenException("user is not found");  
+            }
+            var (jwtSecurityToken, newToken) = GenerateJWTToken(user);
+            
+            //token is expire or not
+            //generate refreshtoken
+            var response = new JwtAuthResult();
+            
+            response.AccessToken = newToken;
+            var refreshTokenResult = new RefreshToken();
+            refreshTokenResult.UserName= token.Claims.FirstOrDefault(x => x.Type == nameof(UserClaimModel.UserName)).Value;
+            refreshTokenResult.TokenString = refreshToken;
+            refreshTokenResult.ExpireAt = userRefreshToken.ExpiryDate;
+            response.refreshToken = refreshTokenResult;
+            return response;    
+
+
+        }
+        private JwtSecurityToken ReadJWTToken(string accessToken)
+        {
+            if (string.IsNullOrEmpty(accessToken))
+            {
+                throw new ArgumentNullException(nameof(accessToken));
+            }
+            var handler = new JwtSecurityTokenHandler();
+            var response = handler.ReadJwtToken(accessToken);
+            return response;
+                
+        }
+
+        public async Task<string> ValidateToken(string accessToken)
+        {
+            var handler = new JwtSecurityTokenHandler();
+     
+            var parameters = new TokenValidationParameters
+            {
+                ValidateIssuer = _jwtSettings.ValidateIssuser,
+                ValidIssuers = new[] { _jwtSettings.Issuser },
+                ValidateIssuerSigningKey = _jwtSettings.ValidateIssuserSigingKey,
+                IssuerSigningKey = new SymmetricSecurityKey(Encoding.ASCII.GetBytes(_jwtSettings.Secret)),
+                ValidAudience = _jwtSettings.Audience,
+                ValidateAudience = _jwtSettings.ValidateAudience,
+                ValidateLifetime = _jwtSettings.ValidateLifeTime,
+            };
+            var validator = handler.ValidateToken(accessToken, parameters, out SecurityToken validatedToken);
+            try
+            {
+                if (validator == null)
+                {
+                    throw new SecurityTokenExpiredException("Invalid token");
+
+                }
+                return "NotExpired";
+
+            }
+            catch (Exception ex) 
+            {
+                return ex.Message;
+            }
+
         }
         #endregion
 
