b7d74de849af0392d95351aa7bbb3f9606ef8180
Rahma Mohamed <116441676+RahmaMohamed211@users.noreply.github.com>
2025-01-20 02:34:35 +0200

    Authorization (#9)
    * Added createRoleEndPoint
    
    * Added DataSeediNforRole
    
    ---------
    
    Co-authored-by: rahma.sw2021 <rahma.sw2021@fci.helwan.edu.eg>

---
+++ SchoolProject.infrastructure/Seeder/UserSeeder.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+using SchoolProject.Data.Entities.Identity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SchoolProject.infrastructure.Seeder
+{
+    public static class UserSeeder
+    {
+        public static async Task SeedAsync(UserManager<User> _userManager)
+        {
+            var usersIsCount= await _userManager.Users.CountAsync();
+            if(usersIsCount <= 0)
+            {
+                var defaultuser = new User()
+                {
+                    UserName="admin",
+                    Email="admin@project.com",
+                    FullName="schoolProject",
+                    Country="Egypt",
+                    PhoneNumber="16464446",
+                    Address="Cairo",
+                    EmailConfirmed=true,
+                    PhoneNumberConfirmed=true,
+                };
+                await _userManager.CreateAsync(defaultuser,"P@ssw0rd");
+                await _userManager.AddToRoleAsync(defaultuser, "Admin");
+            }
+        }
+    }
+}
